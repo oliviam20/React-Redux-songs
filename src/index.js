4742b22d586573e7c73f09c2f3fd424f81d35273
olivia mo <olivia.mo@bigtincan.com>
2019-11-25 18:47:13 +1100

    add Provider and wrap App in Provider passing in the redux store

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App/App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+import App from './components/App/App';
+// importing index.js automatically from reducers folder (only works if file is named index.js)
+import reducers from './reducers';
+
+ReactDOM.render(
+  // Wrap App component with Provider component so our App can access the store
+  // create a redux store using createStore, passing in our reducers (songs, selectedSong) exported from the reducer file
+  <Provider store={createStore(reducers)}>
+    <App />
+  </Provider>,
+  document.querySelector('#root')
+);
